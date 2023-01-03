9e5838fa7aa1e384c7a95ee7075c2f087f248e36
Bruno Otávio Souza de Ávila <92824908+BrunoSA89@users.noreply.github.com>
2023-01-03 15:21:19 -0300

    Navigation realizado em todas as telas

--- app/src/main/java/com/example/exercicio27_12app/FragmentFirst.kt
+++ app/src/main/java/com/example/exercicio27_12app/FragmentFirst.kt
@@ -1,4 +1,26 @@
 package com.example.exercicio27_12app
 
-class FragmentFirst {
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
+class FragmentFirst : Fragment(R.layout.fragment_first) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.button1).setOnClickListener {
+            findNavController().navigate(R.id.action_login_fragment_four)
+        }
+
+        view.findViewById<Button>(R.id.button2).setOnClickListener {
+            findNavController().navigate(R.id.action_login_fragment_second)
+        }
+
+        view.findViewById<Button>(R.id.button3).setOnClickListener {
+            findNavController().navigate(R.id.action_login_fragment_five)
+        }
+    }
 }
